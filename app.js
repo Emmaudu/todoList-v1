8870d004af5631196cf77e269920c145b8e1d60a
Emmaudu <emmanuel.uduebholo@stu.cu.edu.ng>
2019-07-20 08:33:22 -0400

    Final before introducing EJS Templating

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 var today = new Date();
-var currentDay = today.getDay;
+var currentDay = today.getDay();
 
 app.get('/', (req, res)=>{
 
-    if (currentDay===0 || currentDay===6){
+    if (currentDay===6 || currentDay===0){
         res.sendFile(__dirname + "/weekend.html");
     }else{
         res.sendFile(__dirname + "/weekday.html");
-    };
+    }
 
-    res.send('<h1>Welcome</h1>');
+   
 });
 
 
