be434dd5bfc6d967bd9984698fe03b2a14b64c5b
Emmaudu <emmanuel.uduebholo@stu.cu.edu.ng>
2019-07-20 09:52:48 -0400

    Introduced toLocaleDateString inplace of ifElse

--- app.js
+++ app.js
@@ -9,32 +9,20 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 
 var today = new Date();
-var currentDay = today.getDay();
+// var currentDay = today.getDay();
+var options={
+    weekday: 'long',
+    month: 'long',
+    year: 'numeric',
+    day:'numeric'
+};
+
+var dateToString = today.toLocaleDateString('en-US', options);
 
 app.get('/', (req, res)=>{
-
-    if (currentDay===0){
-        res.render('list', {todaynow : 'Sunday'});
-        
-    }else if(currentDay===1){
-        res.render('list', {todaynow : 'Monday'});
-
-    }else if(currentDay===2){
-        res.render('list', {todaynow : 'Tuesday'});
-
-    }else if(currentDay===3){
-        res.render('list', {todaynow : 'Wednesday'});
-
-    }else if(currentDay===4){
-        res.render('list', {todaynow : 'Thursday'});
-
-    }else if(currentDay===5){
-        res.render('list', {todaynow : 'Friday'});
-
-    }else if(currentDay===6){
-        res.render('list', {todaynow : 'Saturday'});
-
-    }
+    //Another Way
+    res.render('list', {todaynow: dateToString});
+ 
 
    
 });
