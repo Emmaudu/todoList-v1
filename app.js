f02ab40c65a65b7232bdcd7699d1d56c218b9867
Emmaudu <emmanuel.uduebholo@stu.cu.edu.ng>
2019-07-20 08:48:12 -0400

    updated with Templates EJS

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view-engine', 'ejs');
+
 const port = process.env.PORT || 3000;
 
 var today = new Date();
@@ -9,10 +13,27 @@ var currentDay = today.getDay();
 
 app.get('/', (req, res)=>{
 
-    if (currentDay===6 || currentDay===0){
-        res.sendFile(__dirname + "/weekend.html");
-    }else{
-        res.sendFile(__dirname + "/weekday.html");
+    if (currentDay===0){
+        res.render('list', {todaynow : 'Sunday'});
+        
+    }else if(currentDay===1){
+        res.render('list', {todaynow : 'Monday'});
+
+    }else if(currentDay===2){
+        res.render('list', {todaynow : 'Tuesday'});
+
+    }else if(currentDay===3){
+        res.render('list', {todaynow : 'Wednesday'});
+
+    }else if(currentDay===4){
+        res.render('list', {todaynow : 'Thursday'});
+
+    }else if(currentDay===5){
+        res.render('list', {todaynow : 'Friday'});
+
+    }else if(currentDay===6){
+        res.render('list', {todaynow : 'Saturday'});
+
     }
 
    
